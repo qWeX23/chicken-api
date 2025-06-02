4846115d84059b0d9f388740832cf0bd0ce0db81
Ben Churchill <bchurchill@paytient.com>
2025-06-02 13:25:08 -0500

    .

--- src/main/kotlin/co/qwex/chickenapi/controller/BreedController.kt
+++ src/main/kotlin/co/qwex/chickenapi/controller/BreedController.kt
@@ -3,6 +3,7 @@ package co.qwex.chickenapi.controller
 import mu.KotlinLogging
 import org.apache.commons.text.similarity.LevenshteinDistance
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
