8ef3c336127d46e32ad32f7ad34f6c8142f970eb
Benjamin Churchill <bjc9001@gmail.com>
2025-07-21 08:01:34 -0500

    style: Apply spotless formatting

--- src/main/kotlin/co/qwex/chickenapi/ChickenApiApplication.kt
+++ src/main/kotlin/co/qwex/chickenapi/ChickenApiApplication.kt
@@ -3,8 +3,8 @@ package co.qwex.chickenapi
 import co.qwex.chickenapi.repository.db.GoogleSheetsConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
 @Import(GoogleSheetsConfig::class)
