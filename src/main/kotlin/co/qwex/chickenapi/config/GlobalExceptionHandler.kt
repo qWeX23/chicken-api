07f670c437c10b0502ad2ea457e3184a92c55e97
Benjamin Churchill <bjc9001@gmail.com>
2026-02-05 08:37:45 -0600

    Handle missing static resources (#65)

--- src/main/kotlin/co/qwex/chickenapi/config/GlobalExceptionHandler.kt
+++ src/main/kotlin/co/qwex/chickenapi/config/GlobalExceptionHandler.kt
@@ -5,11 +5,19 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.resource.NoResourceFoundException
 
 private val log = KotlinLogging.logger {}
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+    @ExceptionHandler(NoResourceFoundException::class)
+    fun handleNoResourceFound(ex: NoResourceFoundException): ResponseEntity<Map<String, String>> {
+        log.warn(ex) { "Static resource not found" }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(mapOf("error" to "Not found"))
+    }
+
     @ExceptionHandler(Exception::class)
     fun handleException(ex: Exception): ResponseEntity<Map<String, String>> {
         log.error(ex) { "Unhandled exception" }
